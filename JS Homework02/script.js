d95724b06a568d599db0bd4c786581010eee4b7a
dejantemelkovski <119764356+dejantemelkovski@users.noreply.github.com>
2022-12-09 13:40:57 +0100

    Add files via upload

---
+++ JS Homework02/script.js
@@ -0,0 +1,56 @@
+year = 1986;
+number = 4;
+percent = 12;
+let chineseZodiac=(year-number)%percent;
+
+console.log ('In the Chinese Zodiac');
+
+    if(chineseZodiac===0) {
+    console.log ('You are a Rat')
+    };
+
+    if(chineseZodiac===1) {
+        console.log ('You are a Ox')
+    };
+
+    if(chineseZodiac===2) {
+        console.log ('You are a Tiger')
+    };
+
+    if(chineseZodiac===3) {
+        console.log ('You are a Rabbit')
+    };
+
+    if(chineseZodiac===4) {
+        console.log ('You are a Dragon')
+    };
+
+    if(chineseZodiac===5) {
+        console.log ('You are a Snake')
+    };
+
+    if(chineseZodiac===6) {
+        console.log ('You are a Horse')
+    };
+
+    if(chineseZodiac===7) {
+        console.log ('You are a Goat')
+    };
+
+    if(chineseZodiac===8) {
+        console.log ('You are a Monkey')
+    };
+
+    if(chineseZodiac===9) {
+        console.log ('You are a Rooster')
+    };
+
+    if(chineseZodiac===10) {
+        console.log ('You are a Dog')
+    };
+
+    if(chineseZodiac===11) {
+        console.log ('You are a Pig')
+    };
+    
+
