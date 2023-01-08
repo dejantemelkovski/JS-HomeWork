dc30f6fa2134134c26b75119218908a4ba816be5
dejantemelkovski <119764356+dejantemelkovski@users.noreply.github.com>
2023-01-08 14:00:15 +0100

    Delete JS HomeWork 04 directory

--- JS HomeWork 04/JS HomeWork04-5/script.js
+++
@@ -1,3 +0,0 @@
-let arrayOfNumbers = [4, 12, 62, 70, -10];
-let sum = Math.min(...arrayOfNumbers) + Math.max(...arrayOfNumbers);
-console.log(`Min : ${Math.min(...arrayOfNumbers)}, Max : ${Math.max(...arrayOfNumbers)}, Sum: ${sum}`);
--- JS HomeWork 04/JS HomeWork04-6/script.js
+++
@@ -1,6 +0,0 @@
-let studensNames = ['1.Kire', '2.Pere', '3.Kole', '4.Stojna'];
-let studentsLastnames = ['Peov', 'Peovski', 'Peovic', 'Peovich'];
-
-let namesLastnames = studensNames.map((e, i) => [e + (" ") + studentsLastnames[i]]);
-
-console.log(namesLastnames);
